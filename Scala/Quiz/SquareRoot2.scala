856e68fa616139b569b87171ea82f8350294ea89
Sebastian Kim <sebastian.kim@omg.com.au>
2012-08-28 01:37:18 +1000

    [Scala] Add another square root practice

---
+++ Scala/Quiz/SquareRoot2.scala
@@ -0,0 +1,24 @@
+/**
+ * Square Roots by Newton’s Method
+ */
+
+object SquareRoot2 {
+  val tolerance = 0.0001
+
+  def main(args: Array[String]) {
+    println(sqrt(2))
+  }
+
+  def sqrt(x: Double) = fixedPoint(y => (y + x/y) / 2)(1.0)
+
+  def fixedPoint(f: Double => Double)(firstGuess: Double) = {
+    def iterate(guess: Double): Double = {
+      val next = f(guess)
+      println(next)
+      if(isCloseEnough(guess, next)) next else iterate(next)
+    }
+    iterate(firstGuess)
+  }
+
+  def isCloseEnough(x: Double, y: Double) = ((x - y) / x).abs < tolerance
+}
