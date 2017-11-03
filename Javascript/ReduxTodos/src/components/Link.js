186917e56ed719f3600b09eca1891503bed5e6b7
Sebastian Kim <sydneyitguy@gmail.com>
2017-11-04 02:39:02 +0900

    Add Redux Todo List tutorial

---
+++ Javascript/ReduxTodos/src/components/Link.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Link = ({ active, children, onClick }) => {
+  if (active) {
+    return (<span>{children}</span>)
+  }
+
+  return (
+    <button
+      onClick={e => {
+        e.preventDefault()
+        onClick()
+      }}>
+      {children}
+    </button>
+  )
+}
+
+Link.propTypes = {
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+export default Link
